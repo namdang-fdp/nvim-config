488cc766c283d22051d27982ffdb391630eb626e
Đặng Phương Nam <namdangcoder@gmail.com>
2026-07-28 19:26:34 +0700

    feat: revert space f and space e behavior

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -4,10 +4,11 @@ return {
 		version = "*",
 		cmd = { "ToggleTerm", "TermExec" },
 		keys = {
-			{ "<leader>tt", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
-			{ "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Float terminal" },
-			{ "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Horizontal terminal" },
-			{ "<leader>tv", "<cmd>ToggleTerm direction=vertical size=80<cr>", desc = "Vertical terminal" },
+			{ [[<C-\>]], "<cmd>1ToggleTerm direction=float<cr>", desc = "Toggle floating terminal" },
+			{ "<leader>tt", "<cmd>1ToggleTerm direction=float<cr>", desc = "Toggle terminal" },
+			{ "<leader>tf", "<cmd>1ToggleTerm direction=float<cr>", desc = "Float terminal" },
+			{ "<leader>th", "<cmd>2ToggleTerm direction=horizontal<cr>", desc = "Horizontal terminal" },
+			{ "<leader>tv", "<cmd>3ToggleTerm direction=vertical size=80<cr>", desc = "Vertical terminal" },
 		},
 		opts = {
 			size = function(term)
@@ -24,19 +25,35 @@ return {
 			insert_mappings = false,
 			terminal_mappings = false,
 			persist_size = true,
+			persist_mode = false,
 			direction = "float",
 			close_on_exit = true,
 			shell = vim.o.shell,
 			float_opts = {
 				border = "rounded",
 				winblend = 0,
+				width = function()
+					return math.floor(vim.o.columns * 0.8)
+				end,
+				height = function()
+					return math.floor(vim.o.lines * 0.7)
+				end,
 			},
+			on_open = function(term)
+				if term.id == 1 then
+					vim.keymap.set("t", [[<C-\>]], "<cmd>1ToggleTerm direction=float<cr>", {
+						buffer = term.bufnr,
+						desc = "Toggle floating terminal",
+					})
+				end
+				vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]], {
+					buffer = term.bufnr,
+					desc = "Exit terminal mode",
+				})
+			end,
 		},
 		config = function(_, opts)
 			require("toggleterm").setup(opts)
-			vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]], {
-				desc = "Exit terminal mode",
-			})
 		end,
 	},
 	{
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,15 @@
 -- NEO-TREE - FILE EXPLORER
 -- ============================================
 
+local function reveal_current_file()
+	require("neo-tree.command").execute({
+		action = "focus",
+		source = "filesystem",
+		position = "left",
+		reveal = true,
+	})
+end
+
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
@@ -13,6 +22,7 @@ return {
 	cmd = "Neotree",
 	keys = {
 		{ "<leader>e", "<cmd>Neotree toggle filesystem left<cr>", desc = "Toggle file tree" },
+		{ "<leader>f", reveal_current_file, desc = "Reveal file in tree" },
 		{ "<leader>E", "<cmd>Neotree reveal left<cr>", desc = "Reveal current file" },
 		{ "<leader>gs", "<cmd>Neotree float git_status<cr>", desc = "Git status tree" },
 	},
